926f9ec4beab24af38aa0974e3792a1516d178bd
sergiohdljr <sergio.tjf1@gmail.com>
2023-05-08 23:18:54 -0300

    feat: boilerplate next

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-full h-screen bg-red-400 '>
+    <h2>Cadastro de usuários</h2>
+    <button>Cadastre usuários </button>
+    <button>Usuários cadastrados</button>
+    </div>
+  )
+}
