34de63bd6f4916f7c61665760dd34a4424998c85
sergiohdljr <sergio.tjf1@gmail.com>
2023-05-12 13:40:40 -0300

    inicial-setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
+'use client'
 
 export default function Home() {
   return (
     <div className='w-full h-screen bg-red-400 '>
-    <h2>Cadastro de usuários</h2>
+      <main>
+      <h2>Cadastro de usuários</h2>
     <button>Cadastre usuários </button>
-    <button>Usuários cadastrados</button>
+    <button>Usuários cadastrados</button>     
+      </main>
+   
     </div>
   )
 }
