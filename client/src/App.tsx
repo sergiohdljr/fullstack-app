cc175fd7aa3c97be63a018c755ef54ba99e7d94d
sergiohdljr <sergio.tjf1@gmail.com>
2023-05-14 18:52:02 -0300

    new client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,57 @@
-import { Form } from "./components/Form";
+import axios from "axios"
+import { SubmitHandler, useForm } from "react-hook-form"
 
-export function App() {
-  return (
-    <div className="max-w-xl mx-auto w-full">
-      <div className="flex justify-center my-12 ">
-        <div className="w-full lg:w-11/12 bg-white p-5 rounded-lg shadow-xl">
-          <h3 className="pt-4 text-2xl text-center font-bold">Criar conta</h3>
-          <Form />
-        </div>
-      </div>
-    </div>
-  );
+interface Iuser{
+  nome: string,
+  email: string,
+  idade:number,
+  genero:"feminino" | "masculino",
+  cpf: string,
+  telefone: string
 }
+
+function App() {
+
+  const { register,handleSubmit} = useForm<Iuser>()
+
+  const handleUserRegister: SubmitHandler<Iuser> =  (usuario) => {
+    axios.get('http://localhost:3000/').then((res)=>console.log(res.data))
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit(handleUserRegister)}>
+          <label htmlFor="nome">
+            <span>Nome: </span>
+            <input {...register("nome")} type="text" />
+          </label>
+          <label htmlFor="email">
+            <span>Email: </span>
+            <input {...register("email")} type="email" />
+          </label>
+          <label htmlFor="idade">
+            <span>Idade: </span>
+            <input {...register("idade")} type="number"/>
+          </label>
+          <select {...register("genero")}>
+            <option value="masculino">Masculino</option>
+            <option value="feminino">Feminino</option>
+          </select>
+          <label htmlFor="cpf">
+            <span>Cpf: </span>
+            <input {...register("cpf")} type="text"/>
+          </label>
+          <label htmlFor="telefone">
+            <span>Telefone: </span>
+            <input {...register("telefone")} type="text"/>
+          </label>
+        </form>
+        <button type="submit">Cadastrar Usuário</button>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
